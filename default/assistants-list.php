2b6dd5a3fbd669f271a329527a1ce3033f3135f0
vareladev <varela.dev@gmail.com>
2019-08-20 11:43:12 -0600

    exporter complete

--- default/assistants-list.php
+++ default/assistants-list.php
@@ -4,17 +4,10 @@
 <meta charset="UTF-8" />
 <meta http-equiv="content-type" content="application/vnd.ms-excel; charset=UTF-8">
 <script>
-/*var tableToExcel = (function() {
-  var uri = 'data:application/vnd.ms-excel;base64,'
-    , template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--><meta http-equiv="content-type" content="text/plain; charset=UTF-8"/></head><body><table>{table}</table></body></html>'
-    , base64 = function(s) { return window.btoa(unescape(encodeURIComponent(s))) }
-    , format = function(s, c) { return s.replace(/{(\w+)}/g, function(m, p) { return c[p]; }) }
-  return function(table, name) {
-    if (!table.nodeType) table = document.getElementById(table)
-    var ctx = {worksheet: name || 'Worksheet', table: table.innerHTML}
-    window.location.href = uri + base64(format(template, ctx))
-  }
-})();*/
+window.onload = function() {
+	tableToExcel("prueba");
+	window.location.replace("assistants.php");
+};
 
 function tableToExcel(name) {
 	var uri = 'data:application/vnd.ms-excel;base64,'
@@ -66,6 +59,45 @@ function exportTableToExcel(tableID, filename = ''){
 </script>
 </head>
 <body>
+<!-- Pre-loader start -->
+<div class="theme-loader">
+    <div class="ball-scale">
+        <div class='contain'>
+            <div class="ring">
+                <div class="frame"></div>
+            </div>
+            <div class="ring">
+                <div class="frame"></div>
+            </div>
+            <div class="ring">
+                <div class="frame"></div>
+            </div>
+            <div class="ring">
+                <div class="frame"></div>
+            </div>
+            <div class="ring">
+                <div class="frame"></div>
+            </div>
+            <div class="ring">
+                <div class="frame"></div>
+            </div>
+            <div class="ring">
+                <div class="frame"></div>
+            </div>
+            <div class="ring">
+                <div class="frame"></div>
+            </div>
+            <div class="ring">
+                <div class="frame"></div>
+            </div>
+            <div class="ring">
+                <div class="frame"></div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Pre-loader end -->
+
 <?php
 	include('sql-calls.php');
 	/*
@@ -119,15 +151,27 @@ function exportTableToExcel(tableID, filename = ''){
 	
 	//creando query
 	$query = create_query_assistants($_POST["evento"], $join_type,$order_by, $in_condition, $fac_condition );
-	echo $query."<br><br>";
+	
 	//creando tabla
-	get_assist_data(str_replace("<br>", " ", $query), $table_title);
-
-	echo "<input type=\"button\" onclick=\"tableToExcel('asistencia')\" value=\"Export to Excel\">";
-	//echo "<input type=\"button\" onclick=\"exportTableToExcel('tblassistants', filename = 'asistencia')\" value=\"Export to Excel\">";
+	echo '<table border="1" id="tblassistants">';
+		get_assist_data(str_replace("<br>", " ", $query), true, true, $table_title);
+		if(isset($_POST['alumnos'])){
+			//echo "incluir alumnos<br>";
+			$query = create_query_assistants_students($_POST["evento"],1);
+			get_assist_data(str_replace("<br>", " ", $query), false, false, "Listado de alumnos que asistieron al evento");
+		}
+		if(isset($_POST['otros'])){
+			//echo "incluir otros empleados<br>";
+			$query = create_query_assistants_students($_POST["evento"],2);
+			get_assist_data(str_replace("<br>", " ", $query), false, false, "Listado de empleados que no definidos en la base de datos pero que asistieron al evento");
+		}
+	echo '</table>';
+	echo "<input type=\"button\" onclick=\"tableToExcel('asistencia')\" value=\"Export to Excel\">";	
+	
+	
 	
 	//ejecutando query y creando tabla
-	function get_assist_data($consulta, $table_title){
+	function get_assist_data($consulta, $set_title, $set_date, $table_subtitle){
 		include 'sql-open.php';
 		$con->set_charset("utf8");
 		$result = $con->query($consulta);
@@ -135,12 +179,15 @@ function exportTableToExcel(tableID, filename = ''){
 		if($result->num_rows > 0){
 			$fieldcount=mysqli_num_fields($result);
 			
-			//Definiendo tabla
-			echo '<table border="1" id="tblassistants">';
 			//Definiendo tema de la tabla
-			echo '<tr><td colspan="'.$fieldcount.'"><b style="font-size: 20px;">Lista de asistencia a evento: '.get_event_name($_POST["evento"]).'</b><br>
-														<b>Fecha del evento: '.get_evento_date($_POST["evento"]).'</b><br>
-														<b>'.$table_title.'</b><br><br></td></tr>';
+			if ($set_title){
+				echo '<tr><td colspan="'.$fieldcount.'"><b style="font-size: 20px;">Lista de asistencia a evento: '.get_event_name($_POST["evento"]).'</b></tr>';
+			}
+			if ($set_date){
+				echo '<tr><td colspan="'.$fieldcount.'"><b>Fecha del evento: '.get_evento_date($_POST["evento"]).'</b></tr>';
+			}
+			echo '<tr><td colspan="'.$fieldcount.'"><b>'.$table_subtitle.'</b></td></tr>';
+			echo '<tr><td colspan="'.$fieldcount.'">&nbsp;&nbsp;</td></tr>';
 			//definiendo cabecera de la tabla
 			echo '<tr>';
 				echo '<td><center><b>Carnet</b></center></td>';
@@ -171,20 +218,12 @@ function exportTableToExcel(tableID, filename = ''){
 					}
 				echo '</tr>';
 			}
-			echo '</table>';
+			echo '<tr><td colspan="'.$fieldcount.'">&nbsp;&nbsp;</td></tr>';
 		}	
 		include "sql-close.php";
 			
 	}	
-	
-	if(isset($_POST['alumnos'])){
-		echo "incluir alumnos<br>";
-	}
-	if(isset($_POST['otros'])){
-		echo "incluir otros empleados<br>";
-	}
-	
-	
+		
 	/*
 	 * $id_evento [int]: id en db del evento a evaluar
 	 * $join_type [string]: posibles valores: "inner", "left"
@@ -242,6 +281,46 @@ function exportTableToExcel(tableID, filename = ''){
 		$query_inner = $query_inner.") AS history_extended <br>
 										GROUP BY history_extended.carnet, history_extended.nombre, history_extended.codigo_unidad, history_extended.unidad<br>
 										ORDER BY history_extended.".$orderby." ASC;";	
+		return $query_inner;
+	}
+	
+	/*
+	 * $id_evento [int]: id en db del evento a evaluar
+	 * $assist_regex [int]: posibles valores: 1, 2. define el usuario a buscar
+	 */
+	function create_query_assistants_students($id_evento,$assist_regex){	
+		$cond_regex = "";
+		if($assist_regex == 1){
+			$cond_regex = "[0-9]{8}";
+		}
+		else{
+			$cond_regex = "[A-Z][A-Z][0-9]{5}";
+		}
+
+		$array_sesiones = get_sesions_id($id_evento);
+	
+		$query_inner = "SELECT history_extended.carnet, history_extended.nombre, history_extended.codigo_unidad, history_extended.unidad,<br>";
+		
+		$cantidad_sesiones = count($array_sesiones);
+		for($i = 0; $i < $cantidad_sesiones; $i++){
+			$query_inner = $query_inner."SEC_TO_TIME(SUM(history_extended.sesion".($i+1).")) AS sesion".($i+1);
+			if ($i+1 < $cantidad_sesiones)
+				$query_inner = $query_inner.",<br>";
+		}
+		$query_inner = $query_inner."<br>FROM (<br>SELECT e.carnet, e.nombre, '-' AS codigo_unidad, '-' AS unidad, <br>";
+		for($i = 0; $i < $cantidad_sesiones; $i++){
+			$query_inner = $query_inner."case when a.id_sesion = ".$array_sesiones[$i]." THEN TIME_TO_SEC(TIME(a.hora_asistencia)) ELSE 0 END AS sesion".($i+1);
+			if ($i+1 < $cantidad_sesiones)
+				$query_inner = $query_inner.",<br>";
+		}
+		$query_inner = $query_inner."<br>FROM otros_asistentes e INNER JOIN otros_asistencia a<br>
+										ON e.carnet = a.carnet<br>
+										WHERE a.id_evento = ".$id_evento." <br>
+											AND e.carnet REGEXP '".$cond_regex."'<br>";
+		
+		$query_inner = $query_inner.") AS history_extended <br>
+										GROUP BY history_extended.carnet, history_extended.nombre, history_extended.codigo_unidad, history_extended.unidad<br>
+										ORDER BY history_extended.carnet ASC;";	
 		return $query_inner;
 	}
 	
